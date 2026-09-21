59e8edd685e2e5bb773f1523b57b66964f309e58
Gabriel Cristian Circiu <gaci@itu.dk>
2026-09-21 09:25:10 +0200

    feat: Template files
    Added in files from the Game Template directory, from the Examples in the Playdate SDK.

--- Source/main.lua
+++ Source/main.lua
@@ -1,1 +1,10 @@
--- Main script comes here.
+-- Basic template code from one of the examples
+
+local gfx = playdate.graphics
+
+gfx.setColor(gfx.kColorBlack)
+
+function playdate.update()
+    gfx.fillRect(0, 0, 400, 240)
+    playdate.drawFPS(0,0)
+end
