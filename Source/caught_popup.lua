acdaf535e5e225d1e404600ab9e8361becf95f4c
Gabriel Cristian Circiu <gaci@itu.dk>
2026-09-24 09:58:03 +0200

    feat: Fish caught lootboxing anim!
    Not quite done yet, almost there ;)

--- Source/caught_popup.lua
+++ Source/caught_popup.lua
@@ -3,6 +3,7 @@ local gfx = playdate.graphics
 local popupTime = 2000
 local popupSpriteText = nil
 local popupSpriteFrame = nil
+local popupSpriteFishes = nil
 
 function caughtPopup()
     fishCaught = false
@@ -12,7 +13,7 @@ function caughtPopup()
     popupSpriteText = gfx.sprite.new()
     popupSpriteText:setImage(gfx.image.new("SystemAssets/caught_popup_text.png"))
     popupSpriteText:moveTo(200, 120)
-    popupSpriteText:setZIndex(999)
+    popupSpriteText:setZIndex(900)
     popupSpriteText:add()
     
     local textStartPoint = playdate.geometry.point.new(200, -100)
@@ -20,18 +21,53 @@ function caughtPopup()
     local textAnimator = gfx.animator.new(200, textStartPoint, textEndPoint, playdate.easingFunctions.outCubic)
     popupSpriteText:setAnimator(textAnimator)
 
-   popupSpriteFrame = gfx.sprite.new()
-   popupSpriteFrame:setImage(gfx.image.new("SystemAssets/caught_popup_frame.png"))
-   popupSpriteFrame:setScale(0.7)
-   popupSpriteFrame:moveTo(200, 120)
-   popupSpriteFrame:setZIndex(999)
-   popupSpriteFrame:add()
+    popupSpriteFrame = gfx.sprite.new()
+    popupSpriteFrame:setImage(gfx.image.new("SystemAssets/caught_popup_frame.png"))
+    popupSpriteFrame:setScale(0.5)
+    popupSpriteFrame:moveTo(200, 120)
+    popupSpriteFrame:setZIndex(901)
+    popupSpriteFrame:add()
+
+    local fishesImage = gfx.image.new("SystemAssets/caught_popup_fishes.png")
+
+    popupSpriteFishes = gfx.sprite.new()
+    popupSpriteFishes:setImage(fishesImage)
+    popupSpriteFishes:moveTo(200, 130)
+    popupSpriteFishes:setZIndex(902)
+    popupSpriteFishes:add()
+
+    popupSpriteFishesLoop = gfx.sprite.new()
+    popupSpriteFishesLoop:setImage(fishesImage)
+    popupSpriteFishesLoop:moveTo(-200, 130)
+    popupSpriteFishesLoop:setZIndex(902)
+    popupSpriteFishesLoop:add()
+
+    local scrollDuration = 800
+    local fishAnim1 = gfx.animator.new(
+        scrollDuration,
+        playdate.geometry.point.new(200, 130),
+        playdate.geometry.point.new(600, 130),
+        playdate.easingFunctions.outCubic
+    )
+    local fishAnim2 = gfx.animator.new(
+        scrollDuration,
+        playdate.geometry.point.new(-200, 130),
+        playdate.geometry.point.new(200, 130),
+        playdate.easingFunctions.outCubic
+    )
+
+    popupSpriteFishes:setAnimator(fishAnim1)
+    popupSpriteFishesLoop:setAnimator(fishAnim2)
 
     playdate.timer.performAfterDelay(popupTime, function()
         popupSpriteText:remove()
         popupSpriteText = nil
         popupSpriteFrame:remove()
         popupSpriteFrame = nil
+        popupSpriteFishes:remove()
+        popupSpriteFishes = nil
+        popupSpriteFishesLoop:remove()
+        popupSpriteFishesLoop = nil
         isAnimPlaying = false
     end)
 end
